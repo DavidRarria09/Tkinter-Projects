a59b8235abb7b8ddb74afa33470b23d38032715b
DavidRarria09 <91222434+DavidRarria09@users.noreply.github.com>
2025-04-17 14:23:14 +0300

    Added more features like a shop. Organised the project a bit and added things in a way that it will be easier if I want to add more in the future.

--- CLICKER GAME.py
+++ CLICKER GAME.py
@@ -1,6 +1,21 @@
 import tkinter as tk
 
-#instanta ferestrei
+stats = {
+    "clicks":0,
+    "pow":1,
+}
+price = {
+"pow":100
+}
+opened ={
+    "shop" : False
+}
+buttons={
+    "counter":lambda:"Clicks: "+str(stats["clicks"]),
+    "button_p":lambda:"Buy "+str(price["pow"])
+}
+
+#main window
 root = tk.Tk()
 
 root.title("CLICKER GAME")
@@ -9,7 +24,34 @@ root.resizable(width=False, height=False)
 root.iconbitmap('data/icon.ico')
 root.attributes('-fullscreen', False)
 
-def inchidere():
+def buy(stat_key):
+    stats[stat_key]+=1
+    price[stat_key]+=50
+def update_b(key, key_str):
+    key.config(text=buttons[key_str]())
+def update_clicks():
+    stats["clicks"] += stats["pow"]
+def new_window(name):
+    #shop window
+    def close_shop():
+        opened["shop"] = False
+        shop.destroy()
+    if name=="shop" and opened["shop"]==False:
+        opened["shop"]=True
+        shop = tk.Toplevel()
+        shop.title("SHOP")
+        shop.geometry("200x300")
+        shop.iconbitmap("data/icon.ico")
+        shop.resizable(width=False, height=False)
+
+        label_p = tk.Label(shop, text="Buy click power!")
+        label_p.pack()
+        button_p = tk.Button(shop, text="Buy "+str(price["pow"]), command=lambda:(buy("pow"),update_b(button_p,"button_p")))
+        button_p.pack()
+
+        rgb(15,15,0,0,0,0,1, 0, shop)
+        shop.protocol("WM_DELETE_WINDOW", close_shop)
+def close_app():
     print("See you next time!")
     root.destroy()
 def rgb(r1, r2, g1, g2, b1, b2, c, pos, ob):
@@ -33,26 +75,24 @@ def rgb(r1, r2, g1, g2, b1, b2, c, pos, ob):
             c+=1
             pos=0
     ob.after(25, lambda: rgb(sol[0], sol[1], sol[2], sol[3], sol[4], sol[5], c, pos, ob))
-eticheta = (tk.Label(root, text="PRESS THE BUTTON!"))
-eticheta.pack()
 
-cnt=0
-def update():
-    global cnt
-    cnt+=1
-    label.config(text="You pressed the button "+str(cnt)+" times!")
+header = (tk.Label(root, text="PRESS THE BUTTON!"))
+header.pack()
 
-label = tk.Label(root, text="You pressed the button  "+str(cnt)+" times!", bg="white")
-label.pack()
+counter = tk.Label(root, text="Clicks: "+str(stats["clicks"]), bg="white")
+counter.pack()
 
-buton = tk.Button(root, text="Buton", command=update, bg="#ff0000")
-buton.pack()
+button_main = tk.Button(root, text="Buton", command=lambda:(update_clicks(), update_b(counter,"counter")), bg="#99f2f9")
+button_main.pack()
 
-exit_b = tk.Button(root, text="Exit", command=inchidere)
+shop_b = tk.Button(root, text="Shop", bg="#99f2f9", command=lambda:new_window("shop"))
+shop_b.pack()
+
+exit_b = tk.Button(root, text="Exit", command=close_app, bg="#ff4d4d")
 exit_b.pack()
 
 rgb(15,15,0,0,0,0, 1, 0, root)
-root.protocol("WM_DELETE_WINDOW", inchidere)
+root.protocol("WM_DELETE_WINDOW", close_app)
 
 #loop main
 root.mainloop()
